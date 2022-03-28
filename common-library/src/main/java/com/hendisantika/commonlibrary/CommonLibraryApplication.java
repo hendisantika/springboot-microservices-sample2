d0cd2e60eb39c97b72716b24a4ab3d05b4d5892b
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-28 14:05:56 +0700

    #5 - Add set getRestTemplate in common-library module

--- common-library/src/main/java/com/hendisantika/commonlibrary/CommonLibraryApplication.java
+++ common-library/src/main/java/com/hendisantika/commonlibrary/CommonLibraryApplication.java
@@ -2,6 +2,8 @@ package com.hendisantika.commonlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CommonLibraryApplication {
@@ -10,4 +12,8 @@ public class CommonLibraryApplication {
         SpringApplication.run(CommonLibraryApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
