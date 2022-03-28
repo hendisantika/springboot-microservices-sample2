b42b2e08a6b7a1b751671b2a137f926e7ab2a6d4
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-28 14:09:18 +0700

    #6 - Add ApiLoggingFilter in common-library module

---
+++ common-library/src/main/java/com/hendisantika/commonlibrary/filter/ApiLoggingFilter.java
@@ -0,0 +1,28 @@
+package com.hendisantika.commonlibrary.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Filter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-microservices-sample2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/03/22
+ * Time: 14.06
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApiLoggingFilter implements Filter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiLoggingFilter.class);
+    private final String requestIdHeaderName;
+    private final String requestIdMDCParamName;
+
+    public ApiLoggingFilter(String requestIdHeaderName, String requestIdMDCParamName) {
+        this.requestIdHeaderName = requestIdHeaderName;
+        this.requestIdMDCParamName = requestIdMDCParamName;
+    }
+}
