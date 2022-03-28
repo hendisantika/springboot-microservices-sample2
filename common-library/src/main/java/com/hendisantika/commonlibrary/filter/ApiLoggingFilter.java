5dda522efebea5b4cc63200d1d8aa80b69cabe44
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-28 14:14:51 +0700

    #12 - Add BufferedServletInputStream in common-library module

--- common-library/src/main/java/com/hendisantika/commonlibrary/filter/ApiLoggingFilter.java
+++ common-library/src/main/java/com/hendisantika/commonlibrary/filter/ApiLoggingFilter.java
@@ -133,5 +133,43 @@ public class ApiLoggingFilter implements Filter {
         }
     }
 
+    private static final class BufferedServletInputStream extends ServletInputStream {
+
+        private final ByteArrayInputStream bais;
+
+        public BufferedServletInputStream(ByteArrayInputStream bais) {
+            this.bais = bais;
+        }
+
+        @Override
+        public int available() {
+            return this.bais.available();
+        }
+
+        @Override
+        public int read() {
+            return this.bais.read();
+        }
+
+        @Override
+        public int read(byte[] buf, int off, int len) {
+            return this.bais.read(buf, off, len);
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setReadListener(ReadListener readListener) {
+
+        }
+    }
 
 }
