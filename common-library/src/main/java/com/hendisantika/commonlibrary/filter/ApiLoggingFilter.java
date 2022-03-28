ca4fe1d322be247bf748c2860443f537a6d6cb5f
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-28 14:14:24 +0700

    #11 - Add BufferedRequestWrapper in common-library module

--- common-library/src/main/java/com/hendisantika/commonlibrary/filter/ApiLoggingFilter.java
+++ common-library/src/main/java/com/hendisantika/commonlibrary/filter/ApiLoggingFilter.java
@@ -6,8 +6,9 @@ import org.slf4j.MDC;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -84,4 +85,53 @@ public class ApiLoggingFilter implements Filter {
         }
         return typesafeRequestMap;
     }
+
+    @Override
+    public void destroy() {
+    }
+
+
+    private static final class BufferedRequestWrapper extends HttpServletRequestWrapper {
+
+        private ByteArrayInputStream bais = null;
+        private ByteArrayOutputStream baos = null;
+        private BufferedServletInputStream bsis = null;
+        private byte[] buffer = null;
+
+        public BufferedRequestWrapper(HttpServletRequest req) throws IOException {
+            super(req);
+            // Read InputStream and store its content in a buffer.
+            InputStream is = req.getInputStream();
+            this.baos = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int read;
+            while ((read = is.read(buf)) > 0) {
+                this.baos.write(buf, 0, read);
+            }
+            this.buffer = this.baos.toByteArray();
+        }
+
+        @Override
+        public ServletInputStream getInputStream() {
+            this.bais = new ByteArrayInputStream(this.buffer);
+            this.bsis = new BufferedServletInputStream(this.bais);
+            return this.bsis;
+        }
+
+        String getRequestBody() throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(this.getInputStream()));
+            String line = null;
+            StringBuilder inputBuffer = new StringBuilder();
+            do {
+                line = reader.readLine();
+                if (null != line) {
+                    inputBuffer.append(line.trim());
+                }
+            } while (line != null);
+            reader.close();
+            return inputBuffer.toString().trim();
+        }
+    }
+
+
 }
