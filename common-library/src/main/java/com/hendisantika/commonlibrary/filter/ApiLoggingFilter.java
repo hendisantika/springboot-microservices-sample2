c87b75e6bc8cddf639d1cb2f2dc260683d918304
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-28 14:15:15 +0700

    #13 - Add TeeServletOutputStream in common-library module

--- common-library/src/main/java/com/hendisantika/commonlibrary/filter/ApiLoggingFilter.java
+++ common-library/src/main/java/com/hendisantika/commonlibrary/filter/ApiLoggingFilter.java
@@ -1,5 +1,6 @@
 package com.hendisantika.commonlibrary.filter;
 
+import org.apache.commons.io.output.TeeOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -171,5 +172,40 @@ public class ApiLoggingFilter implements Filter {
 
         }
     }
+
+    public class TeeServletOutputStream extends ServletOutputStream {
+
+        private final TeeOutputStream targetStream;
+
+        public TeeServletOutputStream(OutputStream one, OutputStream two) {
+            targetStream = new TeeOutputStream(one, two);
+        }
+
+        @Override
+        public void write(int arg0) throws IOException {
+            this.targetStream.write(arg0);
+        }
+
+        public void flush() throws IOException {
+            super.flush();
+            this.targetStream.flush();
+        }
+
+        public void close() throws IOException {
+            super.close();
+            this.targetStream.close();
+        }
+
+        @Override
+        public boolean isReady() {
+            return false;
+        }
+
+        @Override
+        public void setWriteListener(WriteListener writeListener) {
+
+        }
+    }
+
 
 }
